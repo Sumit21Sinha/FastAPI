44c465a1c64a17141f5b1647a4f829cefd569d79
Sumit <sumit525sinha@gmail.com>
2026-07-11 02:40:58 +0530

    Path+Query+Body response.

--- Intermediate/main.py
+++ Intermediate/main.py
@@ -3,3 +3,27 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+users=[]
+
+class User(BaseModel):
+    id : int
+    name : str
+    age : int
+    password : str
+
+class ResponseModel(BaseModel):
+    id : int
+    name : str
+    age : int
+
+@app.post("/user")
+def create_user(data : User):
+    users.append(data)
+    return {"message": "User created successfully"}
+
+@app.get("/user/{id}", response_model = ResponseModel)
+def user(id: int,  notify : bool = True):
+    for user in users:
+        if user.id == id:
+            return user
+    return {"message": "User does not exist"}
