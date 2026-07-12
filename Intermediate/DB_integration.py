e04081c86defebff3ca00dff14f7b32c4a8aa8c6
Sumit <sumit525sinha@gmail.com>
2026-07-12 13:20:34 +0530

    SQLalchemy integration.

--- Intermediate/DB_integration.py
+++ Intermediate/DB_integration.py
@@ -1,6 +1,29 @@
 from fastapi import FastAPI
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, Column, String, Integer
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 app = FastAPI()
 
+engine = create_engine("sqlite:///test.db", connect_args={"check_same_thread": False})
+SessionLocal = sessionmaker(bind = engine)
+session = SessionLocal()
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    email = Column(String)
+
+Base.metadata.create_all(bind = engine)
+
+@app.post("/users")
+def create_user(id : int, name : str, email : str):
+    user = User(id = id, name = name, email = email)
+    session.add(user)
+    session.commit()
+
+@app.get("/users")
+def get_users():
+    users = session.query(User).all()
+    return users
